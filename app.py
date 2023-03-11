775e23e34abb046c84d2037390dc7128d9e910f3
PranavJore <pranavjore@gmail.com>
2023-03-11 13:36:14 +0530

    FH

--- app.py
+++ app.py
@@ -3,9 +3,26 @@ from gnews_scrape import getNews
 import tracemalloc
 tracemalloc.start()
 import asyncio
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.init_app(app)
+    
+try:
+    db.create_all()
+    print("DB create command")
+except:
+    pass
+
+class blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), unique=True)
+    content = db.Column(db.String(10000))
+    date = db.Column(db.String(100))
 
 @app.route("/news")
 def home():
