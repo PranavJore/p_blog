0d1a9f5cc3c60d1c60d24daa9d333c02e4f6036c
PranavJore <pranavjore@gmail.com>
2023-03-14 20:33:47 +0530

    added cards in index

--- app.py
+++ app.py
@@ -4,25 +4,21 @@ import tracemalloc
 tracemalloc.start()
 import asyncio
 from flask_sqlalchemy import SQLAlchemy
+import datetime
+
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ourBlog.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
-db.init_app(app)
-    
-try:
-    db.create_all()
-    print("DB create command")
-except:
-    pass
 
 class blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=True)
     content = db.Column(db.String(10000))
     date = db.Column(db.String(100))
+
 
 @app.route("/news")
 def home():
@@ -32,11 +28,23 @@ def home():
     combList = list(zip(title, link, auth, time))
     return render_template("news.html", news = combList)
 
-
 @app.route("/")
 def index():
-    
     return render_template("index.html")
+
+@app.route("/addBlog", methods=['GET', 'POST'])
+def addBlog():
+    title = request.form['name']
+    cont = request.form['message']
+    dt=datetime.datetime.now()
+    addBlog = blog(title=title, content=cont, date=dt.strftime("%c"))
+    db.session.add(addBlog)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route("/blogs/")
+def dispBlog():
+    return render_template("showBlog.html", dispBlog = blog.query.all())
 
 
 if __name__ == "__main__":
